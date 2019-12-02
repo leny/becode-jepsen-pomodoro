653f954f4a597f50aec8341dbe1e35e366f51b94
leny <info@flatland.be>
2019-12-02 10:44:36 +0100

    Implement logic

--- src/containers/pomodoro.js
+++ src/containers/pomodoro.js
@@ -6,7 +6,7 @@
  * started at 02/12/2019
  */
 
-import React from "react";
+import React, {useState, useEffect, useCallback} from "react";
 
 import Cyphers from "../components/cyphers";
 import Button from "../components/button";
@@ -24,6 +24,7 @@ const styles = {
     timer: {
         flex: "1 0 auto",
         textAlign: "center",
+        fontFamily: "monospace",
         fontSize: "12.5rem",
         fontWeight: "bold",
     },
@@ -36,20 +37,67 @@ const styles = {
     },
 };
 
+const DEFAULT_TIMER_VALUE = 10; // 25 * 60;
+const NBSP = "\u00a0";
+
 export default () => {
-    const handleStartStop = () => console.warn("start/stop!");
-    const handleReset = () => console.warn("reset!");
+    const [intervalId, setIntervalId] = useState();
+    const [timer, setTimer] = useState(DEFAULT_TIMER_VALUE);
+    const [running, setRunning] = useState(false);
+
+    useEffect(() => {
+        if (running) {
+            const id = setInterval(
+                () => setTimer(t => Math.max(0, t - 1)),
+                1000,
+            );
+            setIntervalId(id);
+        } else {
+            clearInterval(intervalId);
+            setIntervalId(null);
+        }
+
+        return () => {
+            if (intervalId) {
+                clearInterval(intervalId);
+            }
+        };
+    }, [running]);
+
+    useEffect(() => {
+        if (timer === 0) {
+            setRunning(false);
+            console.warn("Show modal!");
+        }
+    }, [timer]);
+
+    const handleStartStop = useCallback(() => setRunning(!running), [running]);
+    const handleReset = useCallback(() => setTimer(DEFAULT_TIMER_VALUE), []);
+
+    let separator = ":";
+
+    if (running) {
+        separator = timer % 2 ? NBSP : ":";
+    }
 
     return (
         <div style={styles.container}>
             <div style={styles.timer}>
-                <Cyphers value={0} />
-                {":"}
-                <Cyphers value={0} />
+                <Cyphers value={Math.floor(timer / 60)} />
+                {separator}
+                <Cyphers value={timer % 60} />
             </div>
             <div style={styles.tools}>
-                <Button label={"Reset"} onClick={handleReset} disabled />{" "}
-                <Button label={"Start"} onClick={handleStartStop} />
+                <Button
+                    label={"Reset"}
+                    onClick={handleReset}
+                    disabled={running}
+                />
+                {NBSP}
+                <Button
+                    label={running ? "Stop" : "Start"}
+                    onClick={handleStartStop}
+                />
             </div>
         </div>
     );
